e0c648bc827a009b6e0e6342219d2dd01d085066
Mohamad Aly <44276872+Alymohamad@users.noreply.github.com>
2020-07-02 16:41:11 +0200

    fix first test

--- DDQNAgent.py
+++ DDQNAgent.py
@@ -3,11 +3,6 @@
 TODO: Parameter namen Refactoren
 """
 import numpy as np  # For numerical fast numerical calculations
-import matplotlib.pyplot as plt  # For making plots
-import pandas as pd  # Deals with data
-import seaborn as sns  # Makes beautiful plots
-from sklearn.preprocessing import StandardScaler  # Testing sklearn
-import tensorflow  # Imports tensorflow
 from tensorflow.keras.optimizers import Adam  # Imports keras
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Activation, MaxPooling2D, Conv2D, Dropout, Flatten
