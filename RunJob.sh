306c19ada2371b8a5d0cf21168c6b9901a7a1eee
Mohamad Aly <44276872+Alymohamad@users.noreply.github.com>
2020-07-04 19:01:44 +0200

    new instance new path

--- RunJob.sh
+++ RunJob.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 LATEST_DEPOLY_DIR=$(ls -td /opt/codedeploy-agent/deployment-root/b1281a69-c307-4449-9d7d-62a3f0935d31/*/deployment-archive/ | head -1)
 python3 "$LATEST_DEPOLY_DIR"/main_keras_ddqn_test.py
 
